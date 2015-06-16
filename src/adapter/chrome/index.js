98116ecb8c84b55eb7dc2ea58f1f1e5ad7cada16
chengyuhui <chengyuhui1@gmail.com>
2015-06-16 16:58:01 +0800

    Use intl-messageformat for i18n

--- src/adapter/chrome/index.js
+++ src/adapter/chrome/index.js
@@ -1,5 +1,14 @@
 var exp = {
-  m: chrome.i18n.getMessage,
+  m: (msgid, data)=>{
+    var IntlMessageFormat = require('intl-messageformat');
+    var msg = chrome.i18n.getMessage(msgid);
+    if(!data || msg.indexOf('{') === -1){
+      return msg;
+    }
+
+    msg = new IntlMessageFormat(msg, chrome.i18n.getUILanguage());
+    return msg.format(data);
+  },
   messaging: require('./messaging'),
   button: require('./button')
 };
