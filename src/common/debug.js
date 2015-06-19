cecd1417c84429440003b2b911f09b7fb59f0a63
chengyuhui <chengyuhui1@gmail.com>
2015-06-20 00:59:01 +0800

    Ignore data event to improve performance

--- src/common/debug.js
+++ src/common/debug.js
@@ -4,7 +4,7 @@
   var emit_ = EventEmitter.prototype.emit;
   EventEmitter.prototype.emit = function(name) {
     var args = Array.prototype.slice.call(arguments);
-    if (!(this === process.stderr && name === 'drain')) {
+    if (!(this === process.stderr && name === 'drain') && name !== 'data') {
       console.debug("Event '%s', arguments: %s",
                     name, inspect(args.slice(1), false, 1));
     }
