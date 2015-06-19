6b778fb9085b37cc6b4f9e1bfbd5ba0755087603
chengyuhui <chengyuhui1@gmail.com>
2015-06-20 00:58:37 +0800

    Merge index and routes

--- src/options/index.js
+++ src/options/index.js
@@ -1,6 +1,27 @@
 // Assets
+var routes;
 require('file?name=options.html!./options.html');
 require('Kimochi.css');
 require('./style.css');
 
-require('./routes');
+////////////
+// Routes //
+////////////
+import { DefaultRoute, Route, HashLocation, run } from 'react-router';
+import App from './App' ;
+import Home from './Home' ;
+import Sites from './Sites' ;
+import Export from './Export';
+
+routes = (
+  <Route path="/" handler={App}>
+    <DefaultRoute handler={Home}/>
+    <Route path="home" handler={Home}/>
+    <Route path="site" handler={Sites}/>
+    <Route path="export" handler={Export}/>
+  </Route>
+);
+
+run(routes, HashLocation, (Root) => {
+  React.render(<Root/>, document.body);
+});
